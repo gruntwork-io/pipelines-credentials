ca81eb6fd9958a21defd3ffe14c4f76594905f63
Lewis Christie <lewis.christie@gmail.com>
2024-08-28 12:25:36 -0600

    make call a get

--- scripts/fetch-credentials.sh
+++ scripts/fetch-credentials.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 : "${PROVIDER_TOKEN:?"PROVIDER_TOKEN must be set"}"
 : "${PIPELINES_TOKEN_PATH:?"PIPELINES_TOKEN_PATH must be set"}"
 
-pipelines_token="$(curl -s -H "Authorization: Bearer $PROVIDER_TOKEN" -XPOST "https://aperture-dev-git-pipelines-api-gruntwork.vercel.app/api/v1/tokens/pat/$PIPELINES_TOKEN_PATH")"
+pipelines_token="$(curl -s -H "Authorization: Bearer $PROVIDER_TOKEN" "https://aperture-dev-git-pipelines-api-gruntwork.vercel.app/api/v1/tokens/pat/$PIPELINES_TOKEN_PATH")"
 
 echo "::add-mask::$pipelines_token"
 
