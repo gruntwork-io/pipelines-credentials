b4ed774331eb72257b053472a727b40c393440d9
Lewis Christie <lewis.christie@gmail.com>
2024-08-28 11:21:56 -0600

    Base url

--- scripts/fetch-credentials.sh
+++ scripts/fetch-credentials.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 : "${PROVIDER_TOKEN:?"PROVIDER_TOKEN must be set"}"
 : "${PIPELINES_TOKEN_PATH:?"PIPELINES_TOKEN_PATH must be set"}"
 
-pipelines_token="$(curl -s -H "Authorization: bearer $PROVIDER_TOKEN" -XPOST "https://app.gruntwork.io/api/v1/tokens/pat/$PIPELINES_TOKEN_PATH")"
+pipelines_token="$(curl -s -H "Authorization: bearer $PROVIDER_TOKEN" -XPOST "https://aperture-dev-git-pipelines-api-gruntwork.vercel.app/api/v1/tokens/pat/$PIPELINES_TOKEN_PATH")"
 
 echo "::add-mask::$pipelines_token"
 
